3bd6e568f8fe6316e0e9da650a427ceb6ea5e7f3
Mocha0924 <fuka.sho0224@gmail.com>
2024-09-11 14:30:58 +0900

    メイン用のボタンスクリプトを作成

---
+++ KougeiGameJam2024/Assets/Scripts/MainGameButtonController.cs
@@ -0,0 +1,83 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainGameButtonController : MonoBehaviour
+{
+    [SerializeField] private Image ButtonImage;
+    [SerializeField] private Color NormalColor;
+    [SerializeField] private Color EnterColor;
+    [SerializeField] private Color ClickColor;
+    [SerializeField] private float SetTime;
+    private Vector3 NormalSize;
+    [SerializeField] private float EnterSize;
+    [SerializeField] private float ClickSize;
+    private MainGameButtonManager buttonManager => MainGameButtonManager.Instance;
+
+    private enum PUSHTYPE
+    { 
+        GameStart,
+        Title,
+    }
+
+    [SerializeField] private PUSHTYPE PushType; 
+
+    private enum DISPLAYTYPE
+    {
+        Title,
+        Credit
+    }
+
+    [SerializeField] private DISPLAYTYPE DisplayType;
+
+    private void Start()
+    {
+        NormalSize = transform.localScale;
+    }
+    public void EnterButton()
+    {
+        if (buttonManager.IsLoad)
+            return;
+        ButtonImage.DOColor(EnterColor, SetTime);
+        transform.DOScale(NormalSize*EnterSize, SetTime);
+        SoundManager.Instance.PlaySE(SoundManager.SEType.ButtonMove);
+    }
+
+    
+
+    public void ExitButton()
+    {
+        if (buttonManager.IsLoad)
+            return;
+        ButtonImage.DOColor(NormalColor, SetTime);
+        transform.DOScale(NormalSize, SetTime);
+    }
+
+    public void ClickButton()
+    {
+        if (buttonManager.IsLoad )
+            return;
+        SoundManager.Instance.PlaySE(SoundManager.SEType.PushButton);
+        buttonManager.IsLoad = true;
+        ButtonImage.DOColor(ClickColor, SetTime);
+        transform.DOScale(NormalSize*ClickSize, SetTime).OnComplete(() =>
+        {
+            ButtonImage.DOColor(NormalColor, SetTime);
+            transform.DOScale(NormalSize, SetTime);
+
+            Debug.Log("Pushed");
+
+            switch (PushType) 
+            { 
+                case PUSHTYPE.GameStart: buttonManager.LoadScene("MainGameScene"); break;
+                case PUSHTYPE.Title: buttonManager.LoadScene("Title"); break;
+
+            }
+
+        });
+       
+       
+    }
+}
---
+++ KougeiGameJam2024/Assets/Scripts/MainGameButtonManager.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System;
+using UnityEngine.UI;
+using Unity.VisualScripting;
+
+public class MainGameButtonManager : MonoBehaviour
+{
+    [SerializeField] private ButtonController[] Buttons;
+    private int Value = -1;
+    [SerializeField] private Image Fade;
+    [NonSerialized] public static MainGameButtonManager Instance;
+    [NonSerialized] public bool IsLoad = false;
+   
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        else
+            Destroy(this.gameObject);
+
+    }
+
+    private void Start()
+    {
+        SoundManager.Instance.PlayBGM(SoundManager.BGMType.Title);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            UpButton(Buttons);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            DownButton(Buttons);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ClickButton(Buttons);
+        }
+    }
+
+    private void UpButton(ButtonController[]buttons)
+    {
+        if (Value == -1)
+            Value = 0;
+
+
+        else
+        {
+            buttons[Value].ExitButton();
+            Value = Value - 1;
+        }
+
+
+        if (Value < 0)
+            Value = buttons.Length-1;
+
+        buttons[Value].EnterButton();
+    }
+
+    private void DownButton(ButtonController[] buttons)
+    {
+        if (Value == -1)
+            Value = 0;
+
+
+        else
+        {
+            buttons[Value].ExitButton();
+            Value = Value + 1;
+        }
+
+
+        if (Value >= buttons.Length)
+            Value = 0;
+
+        buttons[Value].EnterButton();
+    }
+
+    private void ClickButton(ButtonController[] buttons)
+    {
+        if (Value < 0 && Value >= buttons.Length)
+            return;
+
+        Debug.Log("Pushed");
+        SoundManager.Instance.PlaySE(SoundManager.SEType.Win);
+        buttons[Value].ClickButton();
+    }
+    public void LoadScene(string SceneName)
+    {
+        Fade.DOFade(1, 0.5f).OnComplete(() =>
+        {
+            SceneManager.LoadScene(SceneName);
+        });
+    }
+
+    public void SetTitleValue(ButtonController button)
+    {
+        if(Value>=0&&Value<=Buttons.Length)
+            Buttons[Value].ExitButton();
+        for (int i = 0; i <= Buttons.Length; i++)
+        {
+            if (button == Buttons[i])
+            {
+                Value = i;
+                Buttons[Value].EnterButton();
+                return;
+            }
+        }
+    }
+    
+}
